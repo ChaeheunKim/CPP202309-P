0058c1b7ea5b8f7ca22e8fdaadd0e7e09e573de0
chaeheun <alzl7410@gmail.com>
2023-11-26 04:27:12 +0900

    수정

--- sources/financial.cpp
+++ sources/financial.cpp
@@ -1,9 +1,8 @@
 #include <iostream>
 #include <fstream>
-#include <string>
-#include <vector>
 #include <sstream>
-#include <istream>
+#include <vector>
+#include <string>
 using namespace std;
 
 //기능 1
@@ -11,20 +10,25 @@ using namespace std;
 //데이터는 온통청년 사이트 오픈 API 사용 예정
 
 //엑셀 데이터 읽는 클래스
+
+
+
+
 class CSVReader {
 public:
     CSVReader(const std::string& filename, char delimiter = ',') : filename_(filename), delimiter_(delimiter) {}
 
     std::vector<std::vector<std::string>> readData() {
         std::ifstream in(filename_);
-        if (in.fail()) {
-            std::cerr << "File not found" << std::endl;
+        if (!in.is_open()) {
+            std::cerr << "Failed to open file: " << filename_ << std::endl;
             return {};
         }
 
         std::vector<std::vector<std::string>> data;
-        while (in.good()) {
-            std::vector<std::string> row = readRow(in);
+        std::string line;
+        while (std::getline(in, line)) {
+            std::vector<std::string> row = readRow(line);
             if (!row.empty()) {
                 data.push_back(row);
             }
@@ -33,33 +37,57 @@ public:
         in.close();
         return data;
     }
-    //특정 값에 해당하는 데이터 추출 하는 함수
-    void extractAndPrintRowsWithValue(const std::string& columnName, const std::string& value) {
+
+    void extractAndPrintRowsWithValue(const std::string& columnName1, const std::string& value1,
+        const std::string& columnName2, const std::string& value2,
+        const std::string& outputColumn) {
         std::vector<std::vector<std::string>> data = readData();
         if (data.empty()) {
             std::cerr << "Empty data" << std::endl;
             return;
         }
 
-        // 특정 열 인덱스 찾기
-        int columnIndex = -1;
+        // Find the index of the specified columns
+        int columnIndex1 = -1;
+        int columnIndex2 = -1;
         for (size_t i = 0; i < data[0].size(); ++i) {
-            if (data[0][i] == columnName) {
-                columnIndex = static_cast<int>(i);
-                break;
+            if (data[0][i] == columnName1) {
+                columnIndex1 = static_cast<int>(i);
+            }
+            if (data[0][i] == columnName2) {
+                columnIndex2 = static_cast<int>(i);
             }
         }
 
-        if (columnIndex == -1) {
-            std::cerr << "Column not found" << std::endl;
+        if (columnIndex1 == -1 || columnIndex2 == -1) {
+            std::cerr << "One or both columns not found" << std::endl;
             return;
         }
 
-        // 특정한 값 있는 열 출력
+        // Print rows with the specified values in the specified columns
         for (size_t i = 1; i < data.size(); ++i) {  // Skip header row
-            if (columnIndex < data[i].size() && data[i][columnIndex] == value) {
-                for (const auto& cell : data[i]) {
-                    std::cout << "[" << cell << "]" << "\t";
+            if (columnIndex1 < data[i].size() && columnIndex2 < data[i].size() &&
+                data[i][columnIndex1] == value1 && data[i][columnIndex2] == value2) {
+                if (outputColumn == "") {
+                    for (const auto& cell : data[i]) {
+                        std::cout << "[" << cell << "]" << "\t";
+                    }
+                }
+                else {
+                    int outputIndex = -1;
+                    for (size_t j = 0; j < data[0].size(); ++j) {
+                        if (data[0][j] == outputColumn) {
+                            outputIndex = static_cast<int>(j);
+                            break;
+                        }
+                    }
+
+                    if (outputIndex != -1) {
+                        std::cout << "[" << data[i][outputIndex] << "]";
+                    }
+                    else {
+                        std::cerr << "Output column not found" << std::endl;
+                    }
                 }
                 std::cout << std::endl;
             }
@@ -70,56 +98,37 @@ private:
     std::string filename_;
     char delimiter_;
 
-    std::vector<std::string> readRow(std::istream& in) {
-        std::stringstream ss;
-        bool inquotes = false;
+    std::vector<std::string> readRow(const std::string& line) {
+        std::stringstream ss(line);
         std::vector<std::string> row;
 
-        while (in.good()) {
-            char c = in.get();
-            if (!inquotes && c == '"') {
-                inquotes = true;
-            }
-            else if (inquotes && c == '"') {
-                if (in.peek() == '"') {
-                    ss << (char)in.get();
-                }
-                else {
-                    inquotes = false;
-                }
-            }
-            else if (!inquotes && c == delimiter_) {
-                row.push_back(ss.str());
-                ss.str("");
-            }
-            else if (!inquotes && (c == '\r' || c == '\n')) {
-                if (in.peek() == '\n') {
-                    in.get();
-                }
-                row.push_back(ss.str());
-                return row;
-            }
-            else {
-                ss << c;
-            }
+        std::string cell;
+        while (std::getline(ss, cell, delimiter_)) {
+            row.push_back(cell);
         }
 
         return row;
     }
 };
+
+
 //청년 정책 클래스
 class YouthPolicy {
 private:
-	char FieldPolicy;//분야 입력 받는 변수
-	char RegionPolicy;//지역 입력 받는 변수
-	char FieldSelect;//분야 구분 변수
-	char RegionSelect; //지역 구분 변수
+	string FieldPolicy;//분야 입력 받는 변수
+    string RegionPolicy;//지역 입력 받는 변수
+	//char FieldSelect;//분야 구분 변수
+	//char RegionSelect; //지역 구분 변수
 	char Policy;// 정책 변수
 public:
 	void Field();
 	void Region();
 	void PolicyShow();
 };
+//선택 가능 지역 리스트
+string Region[18] = { "중앙","서울","부산","대구","인천","광주","대전","울산","경기","강원","충북","충남","전북","전남","경북","경남","제주","세종" };
+//선택 가능 분야 리스트
+string Field[5] = { "일자리","주거","교육","복지","참여" };
 
 //청년 정책 분야 입력 함수
 void YouthPolicy::Field() {
@@ -138,28 +147,28 @@ void YouthPolicy::Region() {
 
 //입력받은 지역과 분야에 해당하는 정책 출력 함수
 void YouthPolicy::PolicyShow() {
-    CSVReader csvReader("C:/Users/chee0/Downloads/youth_data.csv");
-	//if ((FieldSelect == FieldPolicy) && (RegionSelect == RegionPolicy)) {
-		//Policy = //데이터에서 사용자가 선택한 지역과 분야에  모두 해당하는 정책
-		csvReader.extractAndPrintRowsWithValue("youthPolicy.polyBizSecd", "광주");
-        csvReader.extractAndPrintRowsWithValue("FieldPolicy", "youthPolicy.polyRlmCd");
-	//}
+    CSVReader csvReader("C:/Users/chee0/Downloads/youth_data.csv"); //본인 컴퓨터에 따라 경로 바꿔줘야 함
+    csvReader.extractAndPrintRowsWithValue("youthPolicy.polyBizSecd", "경기", "youthPolicy.polyRlmCd", "일자리", "youthPolicy.polyBizSjnm");
+	
 }
+
+
 int main() {
-    CSVReader csvReader("C:/Users/chee0/Downloads/youth_data.csv");
-
-
+    CSVReader csvReader("C:/Users/chee0/Downloads/youth_data.csv"); //본인 컴퓨터에 따라 경로 바꿔줘야 함
 
 	YouthPolicy yp;
 	while (true) {
         string FieldPolicy;
         string RegionPolicy;
+        yp.Field();
+        yp.Region();
+        if (FieldPolicy == "0" || RegionPolicy == "0") {
+            break;
+        }
+
+        yp.PolicyShow();
         
-		yp.Field();
-		yp.Region();
-		yp.PolicyShow();
-		if ((FieldPolicy == "0") or (RegionPolicy == "0"))
-			break;
+	
 	}
     return 0;
 }
